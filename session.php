cb5f434c76452dfa1a14d7fa8b50c0d4dc161ec8
Muhammad Restu Firmansyah <mrestufirmansyah51@gmail.com>
2025-10-28 17:56:16 +0700

    Delete session.php

--- session.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    session_start(); // variabel global
-    $_SESSION['isLoggedIn'] = true; // menandakan user sudah login, biasanya di fitur 'ingat saya'
-    $_SESSION['username'] = "ADMIN";
-
-
-    header("Location: cekSession.php");
-?>
